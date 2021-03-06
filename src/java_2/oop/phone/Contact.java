0888cfbb22a80ad6f63c86029d9e5497e5853c0e
El-Bromero <brandonaromero@gmail.com>
2021-03-06 17:13:38 -0600

    Finished java_2 phone folder exercises

--- src/java_2/oop/phone/Contact.java
+++ src/java_2/oop/phone/Contact.java
@@ -7,5 +7,47 @@ class Contact {
         - Phone number
 		- Address
 */
+    private String name;
+    private String phoneNumber;
+    private String address;
+
+    public Contact(String name, String phoneNumber, String address) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "name='" + name + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
 }
 
