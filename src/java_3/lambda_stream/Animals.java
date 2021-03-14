bf2ce4b07b777ae1d6596b04d37a6336b5e07350
El-Bromero <brandonaromero@gmail.com>
2021-03-14 14:02:22 -0500

    Finished Animals static methods using streams

--- src/java_3/lambda_stream/Animals.java
+++ src/java_3/lambda_stream/Animals.java
@@ -1,6 +1,7 @@
 package java_3.lambda_stream;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 class Animals {
     private static List<String> animals = new ArrayList<>(List.of("peacoCK","rabbit","chiwawa","OranguTAN","vipeR","cobra","paNDa","bUffalo","DeeR","maLLard","lEOpArD","Platypus","frOg","lEOpArD"));
@@ -12,27 +13,60 @@ class Animals {
     static List<String> capsFirst(boolean mutate) {
         //clean up the animals list so that the first letter is capitalized, and all the other letters are lowercased. Use a stream to accomplish this task.  Also, the 2nd parameter of this function is a boolean.  use this boolean 'flag' to determine whether or not to 'mutate' the original animals array stored as a static class field.  if the flag is set to 'true', mutate the animals and return the animals out of the function.  if it is false, create a copy of the animals, perform your stream operations on the copy, and return the copy of animals out of the function, WITHOUT modifying the original animals array.  
         //ex : Animals.capsFirst(false) => ["Peacock","Rabbit","Chiwawa","Orangutan","Viper","Cobra","Panda","Buffalo","Deer","Mallard"]
-        return animals;
+        if(mutate)
+        {
+            animals = animals.stream().map(n -> n.substring(0,1).toUpperCase() + n.substring(1).toLowerCase()).collect(Collectors.toList());
+            return animals;
+        }
+        else {
+            List<String> animalsCopy = animals;
+            return animalsCopy.stream().map(n -> n.substring(0,1).toUpperCase() + n.substring(1).toLowerCase()).collect(Collectors.toList());
+        }
     }
 
     static String addAnimal(String animal) {
         //add an animal to the animal list.
+        animals.add(animal);
         return animal;
     };
 
     static List<String> lowerFirst(boolean mutate) {
         //lowercase the first letter, and uppercase the rest of the letters, using streams.  Also, depending on the value of the boolean flag 'mutate', mutate the original animals list, or perform your stream operations on a 'copy' of the animals list.  return the list out of hte function in both cases.
-        return animals;
+        if(mutate)
+        {
+            animals = animals.stream().map(n -> n.substring(0,1).toLowerCase() + n.substring(1).toUpperCase()).collect(Collectors.toList());
+            return animals;
+        }
+        else {
+            List<String> animalsCopy = animals;
+            return animalsCopy.stream().map(n -> n.substring(0,1).toLowerCase() + n.substring(1).toUpperCase()).collect(Collectors.toList());
+        }
     }
 
     static List<String> flipAnimals(boolean mutate) {
         //reverse the order of the animals in the animal list.  If the booleaen parameter is true, reverse the static animals array list by mutating the array.  if the mutate boolean is false, flip a 'copy' of the animals list, then return that list of flipped animals, WITHOUT mutating the static animals array. return the flipped list in both cases.
-        return animals;
+        if(mutate)
+        {
+            animals = animals.stream().sorted(Collections.reverseOrder()).collect(Collectors.toList());
+            return animals;
+        }
+        else {
+            List<String> animalsCopy = animals;
+            return animalsCopy.stream().sorted(Collections.reverseOrder()).collect(Collectors.toList());
+        }
     }
 
     static List<String> sortAnimals(boolean mutate) {
     //sort the animals in alphabetical order.  If the booleaen parameter is true, mutate the static animals list.  if the mutate boolean is false, sort a 'copy' of the animals list, then return that list of sorted animals, WITHOUT mutating the static animals array. return the sorted list in both cases.
-        return animals;
+        if(mutate)
+        {
+            animals = animals.stream().sorted().collect(Collectors.toList());
+            return animals;
+        }
+        else {
+            List<String> animalsCopy = animals;
+            return animalsCopy.stream().sorted().collect(Collectors.toList());
+        }
     }
 
 }
